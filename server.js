2722cc763bf4b4cb5f7ac606dd8e175a7d426674
DESKTOP-EVP3BIN\11917 <1191755065@qq.com>
2025-05-06 13:18:51 -0700

    complete change of html to ejs

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 const express = require("express");
 var session = require('express-session')
 const mongoose = require('mongoose');
-
-/*example schema and model
-const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    role: {
-        type: String,
-        enum: ['admin', 'user'],
-        default: 'user'
-    }
-});
-const usersModel = mongoose.model('users', userSchema);
-*/
+const path = require('path');
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -107,9 +95,9 @@ const categoriesModel = mongoose.model('categories', categorySchema);
 
 main().catch(err => console.log(err));
 async function main() {
-    await mongoose.connect('mongodb+srv://learnmongodbrcmrv:BpgT7k079N7U9kgL@cluster0.zznebjg.mongodb.net');
-
     const app = express();
+
+    await mongoose.connect('mongodb+srv://learnmongodbrcmrv:BpgT7k079N7U9kgL@cluster0.zznebjg.mongodb.net');
 
     app.use(session({
         secret: 'keyboard cat',
@@ -117,55 +105,63 @@ async function main() {
         saveUninitialized: true,
         cookie: { secure: false }
     }))
+
     app.use(express.urlencoded({ extended: true }));
     const port = 3000;
 
+    app.use(express.static(path.join(__dirname, 'style')));
+    app.use(express.static(path.join(__dirname, 'src')));
     app.set('view engine', 'ejs')
 
     app.listen(port, () => {
         console.log(`Server is running on http://localhost:${port}`);
     })
+
     app.get('/', (req, res) => {
         res.redirect('/home');
     })
 
-    app.get('/login', (req, res) => {
-        res.sendFile(__dirname + '/login.html');
-    })
-
-    app.post('/login', async (req, res) => {
-        const { username, password } = req.body;
-        const user = await usersModel.findOne({ username: username, password: password });
-
-        if (user) {
-            req.session.user = user;
-            res.redirect('/home');
-        } else {
-            res.status(401).send('Invalid credentials');
-        }
-    })
-
-    const isAuthenticated = (req, res, next) => {
-        if (req.session && req.session.user) {
-            return next();
-        } else {
-            res.redirect('/login');
-        }
-    }
-    app.use(isAuthenticated);
-
     app.get('/home', (req, res) => {
-        console.log(req.session.user);
-        res.render('index.ejs', { username: req.session.user.username, role: req.session.user.role });
+        res.render('index.ejs');
     })
 
-    const isAdmin = (req, res, next) => {
-        if (req.session && req.session.user && req.session.user.role === 'admin') {
-            return next();
-        } else {
-            res.status(403).send('Forbidden');
-        }
-    }
+    // app.get('/login', (req, res) => {
+    //     res.sendFile(__dirname + '/login.html');
+    // })
 
-    app.use(isAdmin);
+    // app.post('/login', async (req, res) => {
+    //     const { username, password } = req.body;
+    //     const user = await usersModel.findOne({ username: username, password: password });
+
+    //     if (user) {
+    //         req.session.user = user;
+    //         res.redirect('/home');
+    //     } else {
+    //         res.status(401).send('Invalid credentials');
+    //     }
+    // })
+
+    // const isAuthenticated = (req, res, next) => {
+    //     if (req.session && req.session.user) {
+    //         return next();
+    //     } else {
+    //         res.redirect('/login');
+    //     }
+    // }
+    // app.use(isAuthenticated);
+
+    // app.get('/home', (req, res) => {
+    //     console.log(req.session.user);
+    //     res.render('index.ejs', { username: req.session.user.username, role: req.session.user.role });
+    // })
+
+    // const isAdmin = (req, res, next) => {
+    //     if (req.session && req.session.user && req.session.user.role === 'admin') {
+    //         return next();
+    //     } else {
+    //         res.status(403).send('Forbidden');
+    //     }
+    // }
+
+    // app.use(isAdmin);
 }
